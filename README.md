cee596030373d763846cdb0a4cf9856987e7288c
MMK <staph103@gmail.com>
2023-12-14 21:59:07 -0500

    got the readme to open with given information

---
+++ README.md
@@ -0,0 +1,24 @@
+# ggg
+
+  ## Description
+ * hghht
+
+  ## Installation
+ * rtftjh
+
+  ## Usage
+ * gjguug
+
+  ## Contribution
+ * uuyyyg
+
+  ## Test Instructions
+ * hgfgyggu
+
+  ## License
+ * Apache 2.0
+
+  ## Questions
+ * Username: tutuuui
+
+  * https://github.com/tutuuui
--- utils/mdGenerator.js
+++ utils/mdGenerator.js
@@ -2,9 +2,9 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "NONE") {
-    return ` ![Github license](https://img.shields.io/badge/license-${license.replace(/ /g,'%20')}-blue.svg)`;
+    return ` ![Github license](https://img.shields.io/badge/license-${license.replace(/ /g, '%20')}-blue.svg)`;
   }
-  else{
+  else {
     return ""
   }
 }
@@ -12,17 +12,37 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  if (license !== "NONE") {
+    return ` ![Github license](https://img.shields.io/badge/license-${license.replace(/ /g, '%20')}-blue.svg)`;
+  }
+  else {
+    return ""
+  }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+
+
+  return `# ${data.title}\n
+  ## Description\n * ${data.descrpt}\n
+  ## Installation\n * ${data.instal}\n
+  ## Usage\n * ${data.usage}\n
+  ## Contribution\n * ${data.contrib}\n
+  ## Test Instructions\n * ${data.test}\n
+  ## License\n * ${data.license}\n
+  ## Questions\n * Username: ${data.username}\n
+  * https://github.com/${data.username}\n`;
+
 }
 
 module.exports = generateMarkdown;
+
