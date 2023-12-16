07e95e29193739f40a8fe103451e35f3b068760d
MMK <staph103@gmail.com>
2023-12-15 22:55:25 -0500

    worked on formatting the readme in a literal format

--- README.md
+++ README.md
@@ -1,44 +1,35 @@
-# hello ![Github license](https://img.shields.io/badge/license-Apache%202.0-blue.svg)
- 
-
-  ## Table of Contents
- * undefined
-
-
-  * [Description](#description)
-
- * [Installation](#Installation)
-
- * [Usage-Information](#Usage)
-
- * [Contribution-Guidelines](#contribution)
-
- * [Test-Instructions](#test-instructions)
-
- * [License](#license)
-
- * [Questions](#questions)
-
+# sdf  ![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
 
   ## Description
-  hello 
+    asdf 
+
+  ## Table of Contents
+
+  * [Installation](#installation) 
+  * [Usage-Information](#usage)
+  * [Contribution](#contribution)
+  * [Test](#tests)
+   * [License](#license)
+  * [Questions](#questions)
+  
 
   ## Installation
-  hello 
-
+    fg 
+    
   ## Usage
- * hello 
+
+   fgsd 
+  
+## License
 
   ## Contribution
-  hello 
 
-  ## Test Instructions
-  hello 
+    sdfg 
+  ## Tests
+    sdf
 
-  ## License
-   ![Github license](https://img.shields.io/badge/license-Apache%202.0-blue.svg) 
+  ## Questions 
 
-  ## Questions
-  Username: heello 
+  Username: sd 
 
-  https://github.com/heello
+  https://github.com/sd
--- utils/mdGenerator.js
+++ utils/mdGenerator.js
@@ -13,41 +13,70 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-
-  const licenses = {
-    // Define the licenses and their corresponding links
-    mit: '[MIT License](https://opensource.org/licenses/MIT)',
-    apache: '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)',
-    gpl: '[GPL License](https://www.gnu.org/licenses/gpl-3.0.html)',
-
-  };
-
-  if (licenses[license]) {
-    return licenses[license];
-  } else {
-    return '';
+  // Define the licenses and their corresponding links
+  if (license !== "NONE") {
+    return ` * [License](#license)`;
   }
-}
+  else {
+    return ""
+  }
+
+};
+
+
+
 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+
+  if (license !== "NONE") {
+    return `## License`;
+  }
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 
 
 
-  return `# ${data.title}${renderLicenseBadge(data.license)}\n \n
-  ## Table of Contents\n\n
-  * [Description](#description)\n\n * [Installation](#Installation)\n\n * [Usage-Information](#Usage)\n\n * [Contribution-Guidelines](#contribution)\n\n * [Test-Instructions](#test-instructions)\n\n * [License](#license)\n\n * [Questions](#questions)\n\n
-  ## Description\n  ${ data.descrpt } \n
-  ## Installation\n  ${ data.instal } \n
-  ## Usage\n * ${ data.usage } \n
-  ## Contribution\n  ${ data.contrib } \n
-  ## Test Instructions\n  ${ data.test } \n
-  ## Questions\n  Username: ${ data.username } \n
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
+
+  ## Description
+    ${data.descrpt} 
+
+  ## Table of Contents
+
+  * [Installation](#installation) 
+  * [Usage-Information](#usage)
+  * [Contribution](#contribution)
+  * [Test](#tests)
+  ${renderLicenseLink(data.license)}
+  * [Questions](#questions)
+  
+
+  ## Installation
+    ${data.instal} 
+    
+  ## Usage
+
+   ${data.usage} 
+  
+${renderLicenseSection(data.license)}
+
+  ## Contribution
+
+    ${data.contrib} 
+  ## Tests
+    ${data.test}
+
+  ## Questions 
+
+  Username: ${data.username} \n
   https://github.com/${data.username}\n`;
 
 }
